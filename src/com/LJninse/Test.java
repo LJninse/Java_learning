7e7fa23566bcfc18d765bf021a3a83a4cb759d02
初葉夜澜 <2841164265@qq.com>
2026-02-16 14:58:27 +0800

    日期

--- src/com/LJninse/Test.java
+++ src/com/LJninse/Test.java
@@ -1,6 +1,11 @@
 package com.LJninse;
+
+import java.util.Date;
+
 public class Test {
     public static void main(String[] args) {
-
+        Date date = new Date();
+        String s=String.format("%tc" ,date);
+        System.out.println(s);
     }
     }
