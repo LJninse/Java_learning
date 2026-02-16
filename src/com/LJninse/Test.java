d8152a0cdc35077c191911790c1a4211c87c1c54
初葉夜澜 <2841164265@qq.com>
2026-02-16 15:12:28 +0800

    日期完整

--- src/com/LJninse/Test.java
+++ src/com/LJninse/Test.java
@@ -5,7 +5,11 @@ import java.util.Date;
 public class Test {
     public static void main(String[] args) {
         Date date = new Date();
-        String s=String.format("%tc" ,date);
-        System.out.println(s);
+        String Year=String.format("%tY" ,date);
+        String Month=String.format("%tB" ,date);
+        String Day=String.format("%td" ,date);
+        System.out.println("今年是"+Year+"年");
+        System.out.println("现在是"+Month);
+        System.out.println("今天是"+Day+"号");
     }
     }
